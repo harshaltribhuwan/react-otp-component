c4d31f821c7edc33c8658e00f75e97f59935c583
Harshal Tribhuwan <harshaltribhuwan12345@gmail.com>
2025-04-07 21:21:12 +0530

    added github pages code

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react-otp-component/', // 🔥 Replace with your repo name
   plugins: [react()],
-})
+});
